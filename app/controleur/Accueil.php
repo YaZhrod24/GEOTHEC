7af6982bd5862766aec2e489b1e8a813619904aa
HSL-GliTcheur <hsl.glitcheur@gmail.com>
2026-09-03 17:04:55 +0200

    mvc prêt

--- app/controleur/Accueil.php
+++ app/controleur/Accueil.php
@@ -7,7 +7,7 @@ class Accueil
         $titre = 'test';
 
         include_once '../app/vue/html/entete.php';
-        include_once '../app/vue/html/vue.php';
+        // include_once '../app/vue/html/vue.php';
         include_once '../app/vue/html/pied.php';
     }
 
